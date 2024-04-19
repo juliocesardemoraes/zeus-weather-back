ca1a6613d5fc44b93b7c6f0008bd21a3b5a0e115
julio cesar <jcmcf2012@gmail.com>
2024-04-19 19:12:10 -0300

    fix: cors maldito

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ import { getWeather } from "./weather/service.js";
 const app = express();
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(express.json());
 
 app.get("/", (req, res) => {
